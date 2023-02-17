728b3e5d5860ae70fa4717bfa15ef0090657b615
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-17 17:06:08 +0530

    Vpc creation

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "Network" {
-  source = "https://github.com/Ramsai35/tf-module-.git"
+  source = "https://github.com/Ramsai35/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
